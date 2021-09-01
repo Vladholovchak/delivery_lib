cf4a76ee8759718c014b6f9c9c20caf78954cbf5
Vlad <vlad.golovchak.1996@gmail.com>
2021-09-01 15:02:05 +0300

    update comparable, remove not used delivery_speed, add pry

--- transport.rb
+++ transport.rb
@@ -16,16 +16,22 @@ class Transport
   end
 
   def <=>(other)
-    if delivery_speed < other.delivery_speed
+    if max_weight < other.max_weight
       -1
-    elsif delivery_speed == other.delivery_speed
-      0
-    elsif delivery_speed > other.delivery_speed
+    elsif max_weight == other.max_weight
+      if compare_max_distance < other.compare_max_distance
+        -1
+      elsif compare_max_distance > other.compare_max_distance
+        1
+      else
+        0
+      end
+    else
       1
     end
   end
 
-  def delivery_speed
-    speed / max_weight
+  def compare_max_distance
+    @max_distance || Float::INFINITY
   end
 end
